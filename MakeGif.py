66ffcd218606dc989866f78f86f2a8768ca05ac9
David <davidkircos@gmail.com>
2013-09-26 00:07:10 -0400

    initial post

--- MakeGif.py
+++ MakeGif.py
@@ -2,30 +2,29 @@ from PIL import Image, ImageSequence, ImageEnhance
 from images2gif import writeGif
 import random, string
 
-def makegif(jpg_file_name):
-	temp_file_name = ''.join(random.sample(string.ascii_letters+string.digits,10))+'.gif'
+def makegif(jpg_file_name, result_file_name):
 	temp_image = Image.open(jpg_file_name)
 	temp_image = temp_image.rotate(-90)
+	temp_image = temp_image.resize((720, 960), Image.ANTIALIAS)
 	
 	original_duration = 0.14
 
 	#generate fames to be put into gif
 	w,h = temp_image.size
 	frames = [temp_image.copy()]
-	mult = 30
+	mult = 38
 	for x in range(1,3):
 		x = x * mult
-		temp_image = temp_image.crop((20,15,w-x*w/h,h-x*h/w))
+		temp_image = temp_image.crop((29,29,w-x*w/h,h-x*h/w))
 		sharpener = ImageEnhance.Sharpness(temp_image.convert('RGB'))
 		temp_image = sharpener.enhance(1)
-		temp_image = temp_image.rotate(2)
 		temp_image = temp_image.resize((w, h), Image.ANTIALIAS)
+		temp_image = temp_image.rotate(2)
 		frames.append(temp_image.copy())
 
-	#write animated gif
-	gif_file_name = 'static/' + ''.join(random.sample(string.ascii_letters+string.digits,10))+'.gif'
+	gif_file_name = result_file_name.rsplit('.', 1)[0] + ".gif"
+	#writes gif to file
 	writeGif(gif_file_name, frames, duration=original_duration, dither=0)
 
-	return gif_file_name[7:]
+	return gif_file_name
 
-print(makeGif("photo (1).JPG"))
