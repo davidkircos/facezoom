09558b46ac005e1d728bc4e817a989c77c82ac6a
David <davidkircos@gmail.com>
2013-09-26 15:15:27 -0400

    uses s3

--- application.py
+++ application.py
@@ -2,10 +2,17 @@ import os, random, string
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from werkzeug import secure_filename
 
+import boto
+
 from MakeGif import makegif
 
 application = Flask(__name__)
 
+S3_PUBLIC_KEY = "AKIAIWHKKSAX2GVRHUZQ"
+S3_PRIVATE_KEY = "mruUXSnyXU6ylrR3/ZAn66ND82YF4Vjkt/KSM5/W"
+
+s3_connection = boto.connect_s3(S3_PUBLIC_KEY, S3_PRIVATE_KEY)
+fz_s3_bucket = s3_connection.get_bucket("fz-images")
 
 #Set application.debug=true to enable tracebacks on Beanstalk log output. 
 #Make sure to remove this line before deploying to production.
@@ -24,13 +31,9 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@application.route('/var')
-def var():
-	return str(os.getcwd())
-
-@application.route('/fz/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(GIF_FOLDER, filename)
+@application.route('/fz/<imagename>')
+def uploaded_file(imagename):
+    return render_template('image.html', imageurl="https://{0}.s3.amazonaws.com/{1}.gif".format(fz_s3_bucket.name, imagename), imagename=imagename)
 
 @application.route('/layout/<name>')
 def layout_files(name):
@@ -38,18 +41,33 @@ def layout_files(name):
 
 @application.route('/', methods=['GET', 'POST'])
 def upload_file():
+    error = None
     if request.method == 'POST':
         upload_file = request.files['file']
         if upload_file and allowed_file(upload_file.filename):
+        	#save upload
             filename = file_name_generator(10) + '.' + upload_file.filename.rsplit('.', 1)[1].lower()
+            filename_wpath = UPLOAD_FOLDER + filename
             upload_file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
-            gif_file_name = makegif(os.path.join(application.config['UPLOAD_FOLDER'], filename), os.path.join((GIF_FOLDER + filename)))
-            return redirect(GIF_FOLDER+gif_file_name[len(GIF_FOLDER):])
-            #return redirect(url_for('fz', filename=gif_file_name[len(GIF_FOLDER):]))
 
-        return 'File extention not allowed! only jpg or jpeg'
+            #writes disk to disk
+            gif_file_name_wpath = makegif(os.path.join(application.config['UPLOAD_FOLDER'], filename), os.path.join((GIF_FOLDER + filename)))
 
-    return render_template('index.html')
+            #upload gif to s3
+            s3_fz_key = boto.s3.key.Key(fz_s3_bucket)
+            s3_fz_key.key = gif_file_name_wpath[len(GIF_FOLDER):]
+            s3_fz_key.set_metadata("Content-Type", 'image/gif')
+            s3_fz_key.set_contents_from_filename(GIF_FOLDER+gif_file_name_wpath[len(GIF_FOLDER):])
+            s3_fz_key.make_public()
+
+            #cleanup files
+            os.remove(filename_wpath)
+            os.remove(gif_file_name_wpath)
+
+            return redirect(GIF_FOLDER+filename.rsplit('.', 1)[0])
+
+        error = "File extension not allowed, use jpg or jpeg."
+    return render_template('index.html', error=error)
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
