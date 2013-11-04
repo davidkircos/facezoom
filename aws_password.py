90a98060300bc9825b1406d7d23be23986c95d41
David <davidkircos@gmail.com>
2013-11-03 23:55:31 -0500

    Hmm, learning git

---
+++ aws_password.py
@@ -0,0 +1,7 @@
+#information to long onto db for images
+db_PUBLIC_KEY = "AKIAIYYUCBQLLRPLAXUA"
+db_PRIVATE_KEY = "Gb/BNZH85VL3+5jgvIqRLKsVHyPAQ9Uz2kB77wXI"
+
+#information to log onto s3 to save the image files
+S3_PUBLIC_KEY = "AKIAJCEUTJCJSKFXGI5Q"
+S3_PRIVATE_KEY = "Bj5sCkncASqtxzsT4wiZ3S3xvYjCt72iBAirzav3"
