9cf47b95e75391f53bb7da04870547fc5d0878ed
David <davidkircos@gmail.com>
2013-11-03 23:43:25 -0500

    removed file

--- aws_password.py
+++
@@ -1,7 +0,0 @@
-#information to long onto db for images
-db_PUBLIC_KEY = "AKIAIULWCPJA6GT3Y2WQ"
-db_PRIVATE_KEY = "kWcj7xo55mm162VnXtm47E4MxNeZTNKo7JpIzrTX"
-
-#information to log onto s3 to save the image files
-S3_PUBLIC_KEY = "AKIAIWHKKSAX2GVRHUZQ"
-S3_PRIVATE_KEY = "mruUXSnyXU6ylrR3/ZAn66ND82YF4Vjkt/KSM5/W"
