8c859ee155a81a601a78253ff2d6256d48fc31af
davidkircos <davidkircos@gmail.com>
2013-11-04 00:22:28 -0500

    First Public Commit

--- aws_password.py
+++
@@ -1,7 +0,0 @@
-#information to long onto db for images
-db_PUBLIC_KEY = "AKIAIQDUDL7PXHFZBS7Q"
-db_PRIVATE_KEY = "kWEL2sYwU5QlZsx0eRfqkdbnmX4MqU+uKwaIXZCw"
-
-#information to log onto s3 to save the image files
-S3_PUBLIC_KEY = "AKIAITBB3OCIULQDZTVA"
-S3_PRIVATE_KEY = "8zNkHN6wh2s9zIbAAkbw97VQRxddEm4rEAsvrq7t"
