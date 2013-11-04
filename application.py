82031e2d34871562be2fcae0010eab867b564ad1
David <davidkircos@gmail.com>
2013-11-03 23:39:07 -0500

    Changed how AWS pswds are stored

--- application.py
+++ application.py
@@ -5,6 +5,9 @@ from fzdb import im_db
 import hashlib, copy
 from StringIO import StringIO
 
+#This is done so i keep the source code on github safely
+from aws_passwords import db_PUBLIC_KEY, db_PRIVATE_KEY, S3_PUBLIC_KEY, S3_PRIVATE_KEY
+
 import boto
 
 from MakeGif import makegif
@@ -15,17 +18,9 @@ application = Flask(__name__)
 #Make sure to remove this line before deploying to production.
 application.debug=False
 
-#information to log onto s3 to save the image files
-S3_PUBLIC_KEY = "AKIAIWHKKSAX2GVRHUZQ"
-S3_PRIVATE_KEY = "mruUXSnyXU6ylrR3/ZAn66ND82YF4Vjkt/KSM5/W"
-
 #setup for the connection to s3
 s3_connection = boto.connect_s3(S3_PUBLIC_KEY, S3_PRIVATE_KEY)
 fz_s3_bucket = s3_connection.get_bucket("fz-images")
-
-#information to long onto db for images
-db_PUBLIC_KEY = "AKIAIULWCPJA6GT3Y2WQ"
-db_PRIVATE_KEY = "kWcj7xo55mm162VnXtm47E4MxNeZTNKo7JpIzrTX"
 
 #connects to the db
 fz_images_db = im_db(db_PUBLIC_KEY, db_PRIVATE_KEY)
